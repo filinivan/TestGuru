70103dd9e5c8c0c3b7fc8ed7d75c4feafd25f257
Ivan Macbook <filin87@gmail.com>
2020-12-17 17:49:07 +0300

    Add index

--- db/migrate/20201217074320_add_user_id_to_tests.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTests < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tests, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_074320) do
+ActiveRecord::Schema.define(version: 2020_12_17_135246) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", limit: 70
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(version: 2020_12_17_074320) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "users_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["users_id"], name: "index_tests_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,4 +68,5 @@ ActiveRecord::Schema.define(version: 2020_12_17_074320) do
   add_foreign_key "passed_tests", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "users_id"
 end
