92efb854cb55b8d9c8285e674427d55ea1ed20b3
Ivan Macbook <filin87@gmail.com>
2020-12-17 10:46:48 +0300

    AddColumn user_id to tests. Rename assocication in belongs_to (test model)

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_214_130_454) do
-  create_table 'answers', force: :cascade do |t|
-    t.string 'body', limit: 70
-    t.integer 'question_id', null: false
-    t.boolean 'correct', default: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['question_id'], name: 'index_answers_on_question_id'
+ActiveRecord::Schema.define(version: 2020_12_17_074320) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body", limit: 70
+    t.integer "question_id", null: false
+    t.boolean "correct", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name', limit: 50
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "name", limit: 50
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'passed_tests', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'test_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['test_id'], name: 'index_passed_tests_on_test_id'
-    t.index ['user_id'], name: 'index_passed_tests_on_user_id'
+  create_table "passed_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_passed_tests_on_test_id"
+    t.index ["user_id"], name: "index_passed_tests_on_user_id"
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'title', limit: 70
-    t.integer 'test_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['test_id'], name: 'index_questions_on_test_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "title", limit: 70
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table 'tests', force: :cascade do |t|
-    t.string 'title', limit: 70, null: false
-    t.integer 'level', default: 0, null: false
-    t.integer 'category_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['category_id'], name: 'index_tests_on_category_id'
+  create_table "tests", force: :cascade do |t|
+    t.string "title", limit: 70, null: false
+    t.integer "level", default: 0, null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username', limit: 70
-    t.string 'password', limit: 10
-    t.integer 'level'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "username", limit: 70
+    t.string "password", limit: 10
+    t.integer "level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'passed_tests', 'tests'
-  add_foreign_key 'passed_tests', 'users'
-  add_foreign_key 'questions', 'tests'
-  add_foreign_key 'tests', 'categories'
+  add_foreign_key "answers", "questions"
+  add_foreign_key "passed_tests", "tests"
+  add_foreign_key "passed_tests", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
