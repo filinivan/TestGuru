62cdfe3c8d264044d20b743f79f78467bf3c3401
Ivan Macbook <filin87@gmail.com>
2020-12-21 12:38:40 +0300

    Not work seed

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,13 @@ class Test < ApplicationRecord
   has_many :questions
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
-  def self.test_names(category_name)
-    Test.joins(:category).where('categories.name = ?', category_name).order('title DESC').pluck(:title)
-  end
+  scope :simple_level, -> { where(level: 0..1) }
+  scope :medium_level, -> { where(level: 2..4) } 
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) } 
+
+  scope :test_names, ->(category_name) { Test.joins(:category).where('categories.name = ?', category_name).order('title DESC').pluck(:title) } 
+
+  # def self.test_names(category_name)
+  #   Test.joins(:category).where('categories.name = ?', category_name).order('title DESC').pluck(:title)
+  # end
 end
