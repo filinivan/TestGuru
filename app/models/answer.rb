20aff98c3b9988c43b1f462d2bf9acb4e970ef6a
Ivan Macbook <filin87@gmail.com>
2020-12-22 10:46:54 +0300

    Temporary commit

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  validates  :body, presence: true
+  validate :limit_of_answers, on: :create
 
   scope :right_answers, -> { where(correct: true) }
+end
+
+private
+
+def limit_of_answers
+    errors.add(:answer) if Answer.where(question_id: question_id).length > 4
 end
