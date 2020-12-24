19e51bdb76928e680a9192d283acd264757676c7
Ivan Macbook <filin87@gmail.com>
2020-12-24 23:06:17 +0300

    Lesson 6 1.1

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  validates  :body, presence: true
+  validates :body, presence: true
   validate :limit_of_answers, on: :create
 
   scope :right_answers, -> { where(correct: true) }
-end
 
 private
 
 def limit_of_answers
-    errors.add(:answer) if Answer.where(question_id: question_id).length > 4
+  errors.add(:answer) if Question.answers.count >= 4
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# categories = Category.create!([{ name: 'ruby' }, { name: 'web' }])
-# tests = Test.create!([{ title: 'One', level: 0, category_id: categories[0].id, user_id: 0 },
-                    #  { title: 'Two', level: 0, category_id: categories[1].id, user_id: 0 }])
-# Category.create!([{ name: 'ruby' }, { name: 'web' }])
-Test.create!(title: 'One', level: 0, category_id: 1, user_id: 1)
+categories = Category.create!([
+  { name: 'ruby' }, 
+  { name: 'web' }
+])
 
+users = User.create!([
+  { username: 'Test_user', password: '124', level: 0 },
+  { username: 'Test_user2', password: '124', level: 2 }
+])
 
-# questions = Question.create([{ title: 'Какой вы изучаете язык программирования?', test_id: tests[0].id }])
-# answers = Answer.create([{ body: 'Ruby', question_id: questions[0].id, correct: true }])
-# users = User.create([{ username: 'Test_user', password: '123', level: 0 }])
-# passed_tests = PassedTest.create(user_id: users[0].id, test_id: tests[0].id, created_at: '2020-12-14 05:43:25.762660', updated_at: '2020-12-14 05:43:25.762660')
+tests = Test.create!([
+  { title: 'Sample test one', level: 1, category_id: categories[0].id, user_id: users[0].id },
+  { title: 'Sample test two', level: 2, category_id: categories[1].id, user_id: users[0].id }
+])
+
+questions = Question.create!([
+  { title: 'Какой вы изучаете язык программирования?', test_id: tests[0].id },
+  { title: 'Какой сегодня год?', test_id: tests[0].id }
+])
+
+answers = Answer.create!([
+  { body: 'Ruby', question_id: questions[0].id, correct: true },
+  { body: 'Ruby2', question_id: questions[0].id, correct: false }
+])
+
+passed_tests = PassedTest.create!(user_id: users[0].id, test_id: tests[0].id, 
+  created_at: '2020-12-14 05:43:25.762660', updated_at: '2020-12-14 05:43:25.762660')
+
