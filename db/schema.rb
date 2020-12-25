c081af03f79f0780b81a514d3b352a184197de85
Ivan Macbook <filin87@gmail.com>
2020-12-25 10:49:45 +0300

    Add composite index in to Test, Add text message on error And some little fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_135246) do
+ActiveRecord::Schema.define(version: 2020_12_25_073953) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", limit: 70
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_135246) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
