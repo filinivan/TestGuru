9525107e7aaa61aa9497504d4b1efd76645ed38c
Ivan Filin <filin87@gmail.com>
2021-05-21 16:16:57 +0300

    All work.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,11 +2,10 @@ module QuestionsHelper
   
   def question_header(question)
     if question.persisted?
-      text = "Edit " + @question.test.title + " Question"
+      "Edit #{ @question.test.title } Question"
     else 
-      text = "Create New " + @question.test.title = " Question"
+      "Create New #{ @question.test.title } Question"
     end
-    return text
   end
   
 end
