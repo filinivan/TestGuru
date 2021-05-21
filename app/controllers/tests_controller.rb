8cac12d792978f561af3efa472db88c0f4e6ca7b
Ivan Filin <filin87@gmail.com>
2021-05-21 15:33:57 +0300

    Fix everything except deleting of questions.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,51 +1,53 @@
 class TestsController < ApplicationController
+    before_action :find_test, only: %i[edit destroy show]
+
     def index
-        @tests = Test.all
+      @tests = Test.all
     end
 
     def show
-        @test = Test.find(params[:id])
-        @questions = @test.questions
+      @questions = @test.questions
     end
 
     def edit
-      @test = Test.find(params[:id])
     end
 
     def new
-        @test = Test.new
+      @test = Test.new
     end
 
     def create
-        @test = Test.new(test_params)
-
-        if @test.save
-            redirect_to @test
-        else 
-            render :new
-        end
+      @test = Test.new(test_params)
+      if @test.save
+        redirect_to @test
+      else 
+        render :new
+      end
     end 
 
     def destroy
-        @test = Test.find(params[:id])
-        @test.destroy
+      @test.destroy
     
-        redirect_to tests_path
+      redirect_to tests_path
     end
 
     def update
-        @test = Test.find(params[:id])
+      @test = Test.find(params[:id])
 
-        if @test.update(test_params)
-            redirect_to @test
-        else 
-            render :edit
-        end  
+      if @test.update(test_params)
+        redirect_to @test
+      else 
+        render :edit
+      end  
     end 
 
     private
 
     def test_params
-        params.require(:test).permit(:title, :level, :category_id, :user_id)
+      params.require(:test).permit(:title, :level, :category_id, :user_id)
+    end
+
+    def find_test
+      @test = Test.find(params[:id])    
     end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,3 +1,12 @@
 module QuestionsHelper
-
+  
+  def question_header(question)
+    if question.persisted?
+      text = "Edit " + @question.test.title + " Question"
+    else 
+      text = "Create New " + @question.test.title = " Question"
+    end
+    return text
+  end
+  
 end
