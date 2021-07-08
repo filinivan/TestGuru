9cb8c964747b54e105888add6d25fb9e30e0c37e
Ivan Filin <filin87@gmail.com>
2021-07-08 15:34:00 +0300

    Add validations in User model + Fix issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
 
+  validates :email, presence: true
+  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  validates :password, confirmation: true
+
   has_secure_password
 
   def tests_with_level(level_num)
