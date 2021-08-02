a193498e0a3328ba590a3a38b25994509a5702b5
Ivan Filin <filin87@gmail.com>
2021-08-02 16:12:08 +0300

    Localize tests and admin/tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
   helper_method :current_user,
                 :logged_in?
 
@@ -13,10 +14,19 @@ class ApplicationController < ActionController::Base
       tests_path
     end
   end
+
+  def default_url_options
+    I18n.default_locale == I18n.locale ? {} : { lang: I18n.locale }
+  end
+
   
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
