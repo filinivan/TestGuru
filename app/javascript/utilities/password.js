bc60447192893b7276b76ecc4f019ee1ca4f249b
Ivan Filin <filin87@gmail.com>
2022-02-23 00:29:40 +0300

    Fix: remove bg if no confirmation

--- app/javascript/utilities/password.js
+++ app/javascript/utilities/password.js
@@ -7,15 +7,24 @@ function checkPasswordConfirmation() {
   var password = document.getElementById('user_password')
   var confirmation = document.getElementById('user_password_confirmation')
 
-  if (password.value == confirmation.value) {
-    password.classList.add('bg-success')
-    confirmation.classList.add('bg-success')
+  if (confirmation.value == "") {
+    password.classList.remove('bg-success')
+    confirmation.classList.remove('bg-success')
     password.classList.remove('bg-danger')
     confirmation.classList.remove('bg-danger')
-  } else if (password.value != confirmation.value){
+  }
+  
+  else if (password.value != confirmation.value){
     password.classList.remove('bg-success')
     confirmation.classList.remove('bg-success')
     password.classList.add('bg-danger')
     confirmation.classList.add('bg-danger')
-  }
+  } 
+  
+  else if (password.value == confirmation.value) {
+    password.classList.add('bg-success')
+    confirmation.classList.add('bg-success')
+    password.classList.remove('bg-danger')
+    confirmation.classList.remove('bg-danger')
+  } 
 }
