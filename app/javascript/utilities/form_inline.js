d5062167892b427f683a4ab89de660fb4a69657c
Ivan Filin <filin87@gmail.com>
2022-03-09 21:40:59 +0300

    Err

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -6,9 +6,34 @@ document.addEventListener('turbolinks:load', function(){
       controls[i].addEventListener('click', formInlineClickHandler)
     }
   }
+
+  var errors = document.querySelector('.resource-errors')
+
+  if (errors) {
+    var resourceId = errors.dataset.resourceId
+    formInlineHandler(resourceId)
+  }
 })
+
 
 function formInlineClickHandler(event) {
   event.preventDefault()
-  console.log('Handle from inline links')
+  var testId = this.dataset.testId
+  formInlineHandler(testId)
+}
+
+function formInlineHandler(testId) {
+  var link = document.querySelector('.form-inline-link[data-test-id="'+ testId +'"]')
+  var testTitle = document.querySelector('.test-title[data-test-id="'+ testId +'"]')
+  var formInline = document.querySelector('.form-inline[data-test-id="'+ testId +'"]')
+
+  if (formInline.classList.contains('hide')) {
+    testTitle.classList.add('hide')
+    formInline.classList.remove('hide')
+    link.textContent = 'Cancel'
+  } else {
+    testTitle.classList.remove('hide')
+    formInline.classList.add('hide')
+    link.textContent = 'Edit'
+  }
 }
