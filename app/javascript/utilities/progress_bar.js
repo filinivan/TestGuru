0607710b6f6250909dfc7b02118cb57156bb86b1
Ivan Filin <filin87@gmail.com>
2022-03-15 12:26:44 +0300

    All work

--- app/javascript/utilities/progress_bar.js
+++ app/javascript/utilities/progress_bar.js
@@ -1,8 +1,10 @@
 document.addEventListener('turbolinks:load', function(){
   var elem = document.getElementById("myBar")
-  var i =  elem.dataset.percent
-
-  var width = i
-  elem.style.width = "#{i}%"
-  console.log(elem.style.width = "#{i}%")
+  
+  if (elem) {
+    var i =  elem.dataset.percent
+    var width = i
+    elem.style.width = i + '%'
+  }
 })
+
