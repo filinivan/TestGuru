61721ff5660027a84017f3125815818fe811b9f4
Ivan Filin <filin87@gmail.com>
2022-03-17 10:58:26 +0300

    Add base functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, path: :gurus, path_names: {sign_in: :login, sign_out: :logout}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tests#index'
+  get 'feedback', to: 'feedback#new'
+  post 'feedback', to: 'feedback#create'
 
   resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
@@ -11,14 +13,14 @@ Rails.application.routes.draw do
     member do
       post :start
     end
-  end  
-  
+  end
+
   resources :test_passages, only: %i[ show update] do
     member do
       get :result
       post :gist
-    end 
-  end 
+    end
+  end
 
   namespace :admin do
     resources :tests do
