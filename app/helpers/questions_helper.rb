a66afb8f5a0d4a6c6ab7e3b87562feffac16f9d4
Ivan Filin <filin87@gmail.com>
2022-03-19 23:58:32 +0300

    Fixed crash on question blank title

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,11 @@
 module QuestionsHelper
   
   def question_header(question)
+    question = question.nil? ? Question.new : question
     if question.persisted?
-      "Edit #{ @question.test.title } Question"
+      "Edit #{ question.test.title } Question"
     else 
-      "Create New #{ @question.test.title } Question"
+      "Create New #{ @test.title } Question"
     end
   end
   
